e5e8ea759456743ad1ea65a5984803824a0bd3e7
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 20:49:45 +0530

    Refactored:Program to Demonstrate File Operations using FileIO

---
+++ NIOFileAPITest.java
@@ -0,0 +1,48 @@
+package com.employeepayroll;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NIOFileAPITest {
+	private static String HOME = System.getProperty("user.home");
+	private static String PLAY_WITH_NIO = "TempPlayGround";
+
+	@Test
+	public void givenPathWhenCheckedThenConfirm() throws IOException {
+
+		// Checks the Home Directory
+		Path homePath = Paths.get(HOME);
+		Assert.assertTrue(Files.exists(homePath));
+
+		// Delete File And Checks if File Exists or Not
+		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		if (Files.exists(playPath))
+			FileUtils.deleteFiles(playPath.toFile());
+		Assert.assertTrue(Files.notExists(playPath));
+
+		Files.createDirectory(playPath);
+		Assert.assertTrue(Files.exists(playPath));
+
+		IntStream.range(1, 10).forEach(cntr -> {
+			Path tempFile = Paths.get(playPath + "/temp" + cntr);
+			Assert.assertTrue(Files.notExists(tempFile));
+			try {
+				Files.createFile(tempFile);
+			} catch (IOException e) {
+			}
+			Assert.assertTrue(Files.exists(tempFile));
+		});
+
+		// List Files , Directories With Extension
+		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+		Files.newDirectoryStream(playPath).forEach(System.out::println);
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+				.forEach(System.out::println);
+	}
+}
