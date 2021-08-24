a7ba5a182050556786e4329c6fad3d8e6665a7e0
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 21:00:08 +0530

    Refactored:Program to Demonstrate File Operations using FileIO

--- NIOFileAPITest.java
+++ NIOFileAPITest.java
@@ -16,19 +16,21 @@ public class NIOFileAPITest {
 	@Test
 	public void givenPathWhenCheckedThenConfirm() throws IOException {
 
-		// Checks the Home Directory
+		// Checks File Exists 
 		Path homePath = Paths.get(HOME);
 		Assert.assertTrue(Files.exists(homePath));
 
-		// Delete File And Checks if File Exists or Not
+		// Delete File And Checks File Not Exists
 		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
 		if (Files.exists(playPath))
 			FileUtils.deleteFiles(playPath.toFile());
 		Assert.assertTrue(Files.notExists(playPath));
 
+		//Create Directory
 		Files.createDirectory(playPath);
 		Assert.assertTrue(Files.exists(playPath));
 
+		// Create File
 		IntStream.range(1, 10).forEach(cntr -> {
 			Path tempFile = Paths.get(playPath + "/temp" + cntr);
 			Assert.assertTrue(Files.notExists(tempFile));
@@ -39,7 +41,7 @@ public class NIOFileAPITest {
 			Assert.assertTrue(Files.exists(tempFile));
 		});
 
-		// List Files , Directories With Extension
+		// List Files , Directories as well as Files With Extension
 		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
