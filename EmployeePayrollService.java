4646304b1dc6c7d6a98faae9396b1189db5eeee2
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 23:45:13 +0530

    Refactored:Program to Store Employee Payroll into a File

---
+++ EmployeePayrollService.java
@@ -0,0 +1,51 @@
+package com.employeepayroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+	public enum IOService { 
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() { }
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	public static void main(String[] args) {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData(null);
+	}
+
+	public void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	public long countEntries(IOService fileIo) {
+
+		if (fileIo.equals(IOService.FILE_IO))
+			return new EmployeePayrollFileService().countEntries();
+		return 0;
+	}
+
+	public void writeEmployeePayrollData(IOService fileIo) {
+		if (fileIo.equals(IOService.CONSOLE_IO))
+			System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+		else if (fileIo.equals(IOService.FILE_IO))
+			new EmployeePayrollFileService().writeData(employeePayrollList);
+
+	}
+}
---
+++ EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+package com.employeepayroll;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.employeepayroll.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Nishu Tyagi", 500000.0),
+				new EmployeePayrollData(2, "Krati Gupta", 550000.0), new EmployeePayrollData(3, "Pravigya", 600000.0) };
+
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+}
